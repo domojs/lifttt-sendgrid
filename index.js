092884d680e40b0510be4c7bb9a52234abfa2966
npenin <nicolas.penin@dragon-angel.fr>
2015-03-30 06:35:09 +0200

    formatting

--- index.js
+++ index.js
@@ -2,20 +2,34 @@ var settings=require('./settings.json')
 
 var sendgrid=require('sendgrid')(settings.userName, settings.password);
 
-module.exports={name:"sendgrid", "trigger":{}, "actions":[{name:"send", fields:[{ name:"to", displayName:"A"},{ name:"subject", displayName:"Sujet"}{ name:"body", displayName:"Message"}], delegate:function(fields){
-        var result= function(fields){
-			try
-			{
-				sendgrid.send($.extend({}, fields, settings), function(err, result){
-					if(err)
-						console.log(err);
-				});
-			}
-			catch (ex)
-			{
-				console.log(ex);
-			}
-        };
-        result.fields=fields;
-        return result;
-}}]}
+module.exports={
+    name:"sendgrid", 
+    "trigger":{}, 
+    "actions":[
+        {
+            name:"send", 
+            fields:[
+                { name:"to", displayName:"A"},
+                { name:"subject", displayName:"Sujet"},
+                { name:"text", displayName:"Message"}
+            ],
+            delegate:function(fields){
+                var result=function(fields){
+                    try
+                    {
+                        sendgrid.send($.extend({}, fields, settings), function(err, result){
+                            if(err)
+                                console.log(err);
+                        });
+                    }
+                    catch (ex)
+                    {
+                        console.log(ex);
+                    }
+                };
+                result.fields=fields;
+                return result;
+            }
+        }
+    ]
+};
