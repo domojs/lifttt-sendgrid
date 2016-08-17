e35fb42d955e20dfdc1b6fdf7579ebe09cab5220
Nicolas Penin <nicolas.penin@dragon-angel.fr>
2016-08-17 14:46:10 +0200

    updated sendgrid dependencies (breaking changes)

--- index.js
+++ index.js
@@ -15,17 +15,25 @@ module.exports={
             ],
             delegate:function(fields){
                 var result=function(fields){
-                    try
-                    {
-                        sendgrid.send($.extend({}, fields, settings), function(err, result){
-                            if(err)
-                                console.log(err);
-                        });
-                    }
-                    catch (ex)
-                    {
-                        console.log(ex);
-                    }
+                    var helper = require('sendgrid').mail
+                    from_email = new helper.Email(settings.from)
+                    to_email = new helper.Email(fields.to)
+                    subject = fields.subject
+                    content = new helper.Content('text/plain', fields.text)
+                    mail = new helper.Mail(from_email, subject, to_email, content)
+
+                    var sg = require('sendgrid')(settings.apiKey);
+                    var request = sg.emptyRequest({
+                        method: 'POST',
+                        path: '/v3/mail/send',
+                        body: mail.toJSON()
+                    });
+
+                    sg.API(request, function(error, response) {
+                        console.log(response.statusCode)
+                        console.log(response.body)
+                        console.log(response.headers)
+                    })
                 };
                 result.fields=fields;
                 return result;
